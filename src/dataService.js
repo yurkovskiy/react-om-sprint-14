851a593648d62c9379bfc7d61d22b1d1393fe40e
Yuriy Bezgachnyuk <yurkovskiy@gmail.com>
2020-11-17 20:26:56 +0200

    fixed bug at dataService.js

--- src/dataService.js
+++ src/dataService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export default class DataServie {
+export default class DataService {
     async getGitHubUser(user) {
         const URI = `https://api.github.com/users/${user}`;
         return await axios.get(URI);
