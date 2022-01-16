b7c8fac20029bc3eb7e8ddea52a45235534f66ff
Yuriy Bezgachnyuk <yurkovskiy@gmail.com>
2022-01-16 22:10:43 +0200

    cleaning up...

--- src/dataService.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default class DataService {
-    async getGitHubUser(user) {
-        const URI = `https://api.github.com/users/${user}`;
-        return await axios.get(URI);
-    }
-}
